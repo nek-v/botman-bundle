a1d8abc273003a760ee158b762b94964415375be
nek <nek@srez.org>
2021-10-09 18:36:43 +0000

    Fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -25,7 +25,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('botman');
         $root = $treeBuilder->getRootNode();
--- tests/DependencyInjection/FacebookConfigurationTest.php
+++ tests/DependencyInjection/FacebookConfigurationTest.php
@@ -91,7 +91,6 @@ class FacebookConfigurationTest extends TestCase
                     ],
                 ],
             ],
-            'The child node "parameters" at path "botman.drivers.facebook" must be configured.'
         );
     }
 
@@ -111,7 +110,6 @@ class FacebookConfigurationTest extends TestCase
                     ],
                 ],
             ],
-            'The child node "token" at path "botman.drivers.facebook.parameters" must be configured.'
         );
     }
 
@@ -131,7 +129,6 @@ class FacebookConfigurationTest extends TestCase
                     ],
                 ],
             ],
-            'The child node "app_secret" at path "botman.drivers.facebook.parameters" must be configured.'
         );
     }
 
@@ -151,7 +148,6 @@ class FacebookConfigurationTest extends TestCase
                     ],
                 ],
             ],
-            'The child node "verification" at path "botman.drivers.facebook.parameters" must be configured.'
         );
     }
 
